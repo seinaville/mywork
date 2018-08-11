48b5cee596dad6b64a0cee440d221491e19a138f
kai <likai@icloud.com>
2018-08-11 19:50:14 +0800

    添加了正文查询和拼接

--- scraping_web.py
+++ scraping_web.py
@@ -11,13 +11,22 @@ class ScrapingWeb():
     ''' 根据scraping_baidu的结果，提取网页中的全部正文。
         Args:
             url : 带提取的网页
-            connection : mongo链接地址 
+            connection : mongo链接地址
         Returns:
             将网页中的正文存入mongodb中
     '''
     __db_to_get = None  # mongo 数据库
     __url = []   # 保存url地址，为列表对象
     __fn = None  # 用于记录程序运行过程
+    __header = {
+        'Accept':
+        'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Encoding': 'gzip,deflate',
+        'Accept-language': 'en-Us,en;q=0.7,zh-CN;q=0.4',
+        'Cache-control': 'max-age=0',
+        'Connection': 'Keep-alive',
+        'User-Agent': 'Mozilla / 5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit / 537.36 (KHTML, like Gecko) Chrome / 68.0.3440.84 Safari / 537.36'
+    }
 
     def __init__(self, **kwargs):
         time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -45,11 +54,22 @@ class ScrapingWeb():
 
     def Scraping_url(self):
         count = 0  # 计数器
-        for url in self.__url[:5]:
-            html = requests.get(url, timeout=5)
-            print('网页响应状况: %d' % (html.status_code))
-            doc = BeautifulSoup(html.text, 'lxml').select('p')
-        print(len(doc))
+        for url in self.__url[:2]:  # 读取网页地址
+            try:
+                html = requests.get(
+                    url, headers=self.__header, timeout=3)  # 请求网页，3秒内响应
+            except requests.exceptions.RequestException:
+                self.__fn.write(''''网页: %s 请求异常\n
+                                网页响应状况: %d \n''' % (url, html.status_code))
+            else:
+                # 读取网页内所有的节点<P>
+                doc = BeautifulSoup(html.text, 'lxml').select('p')
+                text = ''  # 初始化正文存储器
+                for tag in doc:  # 合并正文
+                    text = '%s ，%s' % (text, tag.get_text())
+                print('\n' + text)
+            finally:
+                self.__fn.close()
 
     def __get_url(self, documents):
         # 从documents中提取url地址
