5c6714cbeccb67494b79328475f95df14438eeab
kai <likai@icloud.com>
2018-08-14 16:42:55 +0800

    scraping_html修改mongo保存格式，检测为什么只有900个网页

--- scraping_html.py
+++ scraping_html.py
@@ -54,11 +54,11 @@ class ScrapingWeb():
 
     def Scraping_url(self):
         count = 0  # 计数器
-        for url in self.__url[:10]:  # 读取网页地址
+        for url in self.__url:  # 读取网页地址
             try:
                 html = requests.get(
                     url, headers=self.__header,
-                    timeout=10, allow_redirects=True)  # 请求网页，10秒内响应
+                    timeout=5, allow_redirects=True)  # 请求网页，10秒内响应
                 html.raise_for_status()
             except requests.exceptions.RequestException:
                 self.__output_message('网页: %s 请求异常\n'
@@ -72,11 +72,15 @@ class ScrapingWeb():
                 else:
                     html.encoding = 'utf-8'  # 防止默认编码 ISO-8859-1
                # 保存全部网页
+               title = BeautifulSoup(html.text, 'lxml').title
                 count += 1
                 self.__db_to_get['search_baidu'].html.update_one(
-                    {'_id': html.url},
-                    {'$set': {'text': html.text}},
-                    upsert=True)
+                    {'_id': count},
+                    {'$set': {'title':title,
+                              'text': html.text,
+                              'url': html.url
+                              }
+                    },upsert=True)
         time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         self.__output_message('文字提取处理完毕: \n'
                               '\t共计处理: %d 页 \n'
